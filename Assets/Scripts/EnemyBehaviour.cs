09f8aab9df5442afd6359c5b060c428c5a67840d
David Santos <39455909+DavidSantosDEV@users.noreply.github.com>
2022-11-03 20:41:03 +0000

    Corn enemy

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+
+    public float enemySpeed;
+
+    public float traceSizeGround = 1;
+    public float traceSizeFront = 1;
+
+    public Transform traceGroundPlace;
+    public Transform traceFrontPlace;
+
+    public LayerMask TraceLayers;
+
+    bool bIsFlipped = false;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (HasTrace(traceGroundPlace,Vector2.down,traceSizeGround))
+        {
+            if (HasTrace(traceFrontPlace, (bIsFlipped ? Vector2.left : Vector2.right), traceSizeFront))
+            {
+                FlipEnemy();
+            }
+        }
+        else
+        {
+            FlipEnemy();
+        }
+
+        rbody.velocity =(bIsFlipped? Vector2.left : Vector2.right) * enemySpeed;
+    }
+
+    bool HasTrace(Transform Start, Vector2 direction, float lenght)
+    {
+        Debug.DrawRay(Start.position, direction * lenght, Color.blue);
+        return Physics2D.Raycast(Start.position, direction, lenght, TraceLayers);
+    }
+
+    void FlipEnemy()
+    {
+        gameObject.transform.localScale = bIsFlipped ? Vector3.one : new Vector3(-1, 1, 1);
+
+        bIsFlipped =!bIsFlipped;
+    }
+}
